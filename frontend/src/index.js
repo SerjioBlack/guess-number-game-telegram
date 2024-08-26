697d6d3bb849335938709be07d68ef97be7316cd
Serhii Sheyko <129678725+SerjioBlack@users.noreply.github.com>
2024-08-26 17:57:54 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Використовуйте createRoot
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
